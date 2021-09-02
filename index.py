c2a698eabd276c5c19701071d651738011727320
starc52 <cstarpowful@gmail.com>
2021-09-02 15:27:15 +0530

    optimized for phase 1

--- index.py
+++ index.py
@@ -6,25 +6,16 @@ import os
 import re
 import json
 import nltk
-from nltk import word_tokenize
-from nltk.stem import SnowballStemmer
 import Stemmer
 import pickle as pkl
-from gibberish_detector import detector
+# from gibberish_detector import detector
 from tqdm import tqdm
-
-# http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
 
 PATH_WIKI_XML = '/home/starc/Downloads/'
 PATH_CSV='/home/starc/IRE-Stuff/'
 FILENAME_WIKI = 'data'
-FILENAME_ARTICLES = 'articles.csv'
-FILENAME_REDIRECT = 'articles_redirect.csv'
-FILENAME_TEMPLATE = 'articles_template.csv'
-ENCODING = "utf-8"
 
 
-# Nicely formatted time string
 def hms_string(sec_elapsed):
     h = int(sec_elapsed / (60 * 60))
     m = int((sec_elapsed % (60 * 60)) / 60)
@@ -41,17 +32,12 @@ def strip_tag_name(t):
 
 
 pathWikiXML = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
-pathArticles = os.path.join(PATH_CSV, FILENAME_ARTICLES)
-pathArticlesRedirect = os.path.join(PATH_CSV, FILENAME_REDIRECT)
-pathTemplateRedirect = os.path.join(PATH_CSV, FILENAME_TEMPLATE)
 
 totalCount = 0
-articleCount = 0
-redirectCount = 0
-templateCount = 0
 title = None
 start_time = time.time()
 count_ns=[]
+
 inverted_index={}
 # ss = SnowballStemmer(language='english')
 stemmer = Stemmer.Stemmer('english')
@@ -62,8 +48,7 @@ with open('stopwords.pkl', 'rb') as f:
     stopword_set = pkl.load(f)
     f.close()
 
-stopword_set.add("redirect")
-stopword_set.add("category")
+
 contin=0
 for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
     tname = strip_tag_name(elem.tag)
@@ -87,22 +72,19 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
                 contin+=1
                 continue
             title_1 = title.lower()
-            title = re.sub(r'([^a-zA-Z0-9 ])', ' ', title_1)
-            title_1 = re.sub(r'([ ]+)', ' ', title)
-            title = title_1
-            temp_title = stemmer.stemWords(title.split(" ")) #word_tokenize(title)
+            temp_title = stemmer.stemWords(re.findall(r'[a-zA-Z0-9]+', title_1))
             for i in temp_title:
-                if i not in stopword_set and len(i)<30 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
-                    stemmed_i = i#ss.stem(i)
+                if i not in stopword_set and len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
+                    stemmed_i = i
                     if stemmed_i not in inverted_index.keys():
-                        temp={totalCount:[1,0]}
-                        inverted_index[stemmed_i]=temp
-                        # print(totalCount)
-                    elif stemmed_i in inverted_index.keys() and totalCount not in inverted_index[stemmed_i].keys():
-                        inverted_index[stemmed_i][totalCount] = [1,0]
+                        inverted_index[stemmed_i]={'t':{totalCount:1}}
+                    elif stemmed_i in inverted_index.keys() and 't' not in inverted_index[stemmed_i].keys():
+                        inverted_index[stemmed_i]['t'] = {totalCount:1}
+                    elif stemmed_i in inverted_index.keys() and totalCount not in inverted_index[stemmed_i]['t'].keys():
+                        inverted_index[stemmed_i]['t'][totalCount]=1
                     else:
-                        inverted_index[stemmed_i][totalCount][0]+=1
-            # title = re.sub(r'#REDIRECT |#redirect ', '', str(title))
+                        inverted_index[stemmed_i]['t'][totalCount]+=1
+
         elif tname == 'id' and not inrevision:
             id = int(elem.text)
         elif tname == 'ns':
@@ -110,33 +92,49 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
         elif tname == 'text':
             text = elem.text
             if not text:
-                # print("text:", text)
                 contin+=1
                 continue
             # stringOfRe = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
             text_1 = text.lower()
-            stringOfRe = r"(http|https|ftp|smtp)://(([^ \n]*?),|([^ \n]*?);|([^ \n]*? )|([^ \n]*?|))"
-            text = re.sub(stringOfRe, ' ', text_1)
-            text_1 = re.sub(r'([^a-zA-Z0-9 ])', ' ', text)
-            text = re.sub(r'([ ]+)', ' ', text_1)
-            # print(text)
-            # if len(orig_text)-len(text)<5:
-            #     print(orig_text, text, totalCount)
             
-            temp_text = stemmer.stemWords(text.split(" ")) #word_tokenize(text)
+            string_cite = r"(\{\{cite web\|.*\|.*)(title=[^|]*)(\|.*\}\})"
+            text = re.sub(string_cite, r"\2", text_1)
+            
+            string_image = r"(\[\[File:.*)(\|[^|]*)(\]\])"
+            text_1 = re.sub(string_image, r"\2", text)
+
+            string_garbage = r"(\{\{[^{}\[\]]*\}\})|(\[\[[^{}\[\]]*\]\])"
+            text = re.sub(string_garbage, ' ', text_1)
+
+            stringOfRe = r"(http://|https://|ftp://|smtp://)([a-z0-9.\-]+[.][a-z]{2,4})([^ \n]*\||[^ \n]*;|[^ \n]* |[^ \n]*|)"
+            text_1 = re.sub(stringOfRe, ' ', text)
+            
+            text_split=text_1.split("\n")
+            for i in range(len(text_split)):
+                if len(text_split[i])>2:
+                    if text_split[i][0:3]=="# 2" or text_split[i][0]=='&':
+                        text_split[i]=""
+            text="\n".join(text_split)
+
+            text_1 = re.findall(r'([a-zA-Z0-9]+)', text)
+
+            temp_text = stemmer.stemWords(text_1)
+
             for i in temp_text:
-                if i not in stopword_set and len(i)<30 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
+                if i not in stopword_set and len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                     stemmed_i = i#ss.stem(i)
                     if stemmed_i not in inverted_index.keys():
-                        temp={totalCount:[0,1]}
+                        temp={'b':{totalCount:1}}
                         inverted_index[stemmed_i]=temp
-                    elif stemmed_i in inverted_index.keys() and totalCount not in inverted_index[stemmed_i].keys():
-                        inverted_index[stemmed_i][totalCount] = [0,1]
+                    elif stemmed_i in inverted_index.keys() and 'b' not in inverted_index[stemmed_i].keys():
+                        inverted_index[stemmed_i]['b'] = {totalCount:1}
+                    elif stemmed_i in inverted_index.keys() and totalCount not in inverted_index[stemmed_i]["b"].keys():
+                        inverted_index[stemmed_i]['b'][totalCount]=1
                     else:
-                        inverted_index[stemmed_i][totalCount][1]+=1
+                        inverted_index[stemmed_i]['b'][totalCount]+=1
+
         elif tname == 'page':
             count_ns.append(ns)
-            # print(text)
             orig_text=text
                         
             totalCount += 1
@@ -145,17 +143,16 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
         elem.clear()
 
 
-count=0
-for i in inverted_index.keys():
-    if Detector.is_gibberish(i):
-        print(i)
-        count+=1
-print(count)
+# count=0
+# # for i in inverted_index.keys():
+# #     if Detector.is_gibberish(i):
+# #         count+=1
+# print(count)
 print("continue count:", contin)
-# print(inverted_index.keys())
 print(len(inverted_index.keys()))
+print(inverted_index.keys())
 with open("inverted_index.json", "w") as f:
-    f.write(json.dumps(inverted_index, indent=0, separators=(",", ":")).replace('\n', ''))
+    f.write(json.dumps(inverted_index, indent=0, separators=(",", ":")).replace("\n", ""))
     f.close()
 
 with open("id_correspondence.json", "w") as f:
@@ -164,9 +161,5 @@ with open("id_correspondence.json", "w") as f:
 
 
 elapsed_time = time.time() - start_time
-# print(set(count_ns))
 print("Total pages: {:,}".format(totalCount))
-print("Template pages: {:,}".format(templateCount))
-print("Article pages: {:,}".format(articleCount))
-print("Redirect pages: {:,}".format(redirectCount))
 print("Elapsed time: {}".format(hms_string(elapsed_time)))
---
+++ stop.py
@@ -0,0 +1,38 @@
+import pickle as pkl
+
+with open('stopwords.pkl', 'rb') as f:
+    stopword_set = pkl.load(f)
+    f.close()
+
+stopword_add=['category', 'redirect', 'ref', 'gallery', 'timeline', 'noinclude', 'pre', 'table', 'tr', 'td',
+            'ul', 'li', 'ol', 'dl', 'dt', 'dd', 'menu', 'dir', 'nowiki', 'cite', 'source', 'hiero', 'div', 'font', 'span', 'strong',
+            'strike', 'blockquote', 'tt', 'var', 'sup', 'sub', 'big', 'small', 'center', 'h1', 'h2', 'h3', 'em', 
+            'b', 'i', 'u', 'a', 's', 'p', 'references', 'ref', 'img', 'br', 'hr', 'li', 'dt', 'dd', 'nbsp', 
+            'iexcl', 'cent', 'pound', 'curren', 'yen', 'brvbar', 'sect', 'uml', 'copy', 'ordf', 'laquo', 'not', 'shy',
+            'reg', 'macr', 'deg', 'plusmn', 'sup2', 'sup3', 'acute', 'micro', 'para', 'middot', 'cedil', 'sup1', 'ordm', 
+            'raquo', 'frac14', 'frac12', 'frac34', 'iquest', 'agrave', 'aacute', 'acirc', 'atilde', 'auml', 'aring', 'aelig', 
+            'ccedil', 'egrave', 'eacute', 'ecirc', 'euml', 'igrave', 'iacute', 'icirc', 'iuml', 'eth', 'ntilde',
+            'ograve', 'oacute', 'ocirc', 'otilde', 'ouml', 'times', 'oslash', 'ugrave', 'uacute', 'ucirc', 'uuml', 'yacute', 
+            'thorn', 'szlig', 'agrave', 'aacute', 'acirc', 'atilde', 'auml', 'aring', 'aelig', 'ccedil', 'egrave', 'eacute', 
+            'ecirc', 'euml', 'igrave', 'iacute', 'icirc', 'iuml', 'eth', 'ntilde', 'ograve', 'oacute', 'ocirc', 'otilde', 
+            'ouml', 'divide', 'oslash', 'ugrave', 'uacute', 'ucirc', 'uuml', 'yacute', 'thorn', 'yuml', 'fnof', 'alpha', 
+            'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu', 'nu', 'xi', 
+            'omicron', 'pi', 'rho', 'sigma', 'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega', 'alpha', 'beta', 'gamma', 
+            'delta', 'epsilon', 'zeta', 'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu', 'nu', 'xi', 'omicron', 'pi', 
+            'rho', 'sigmaf', 'sigma', 'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega', 'thetasym', 'upsih', 'piv', 'bull', 
+            'hellip', 'prime', 'Prime', 'oline', 'frasl', 'weierp', 'image', 'real', 'trade', 'alefsym', 'larr', 'uarr', 
+            'rarr', 'darr', 'harr', 'crarr', 'larr', 'uarr', 'rarr', 'darr', 'harr', 'forall', 'part', 'exist', 'empty', 
+            'nabla', 'isin', 'notin', 'ni', 'prod', 'sum', 'minus', 'lowast', 'radic', 'prop', 'infin', 'ang', 'and', 
+            'or', 'cap', 'cup', 'int', 'there4', 'sim', 'cong', 'asymp', 'ne', 'equiv', 'le', 'ge', 'sub', 'sup',
+            'nsub', 'sube', 'supe', 'oplus', 'otimes', 'perp', 'sdot', 'lceil', 'rceil', 'lfloor', 'rfloor', 'lang',
+            'rang', 'loz', 'spades', 'clubs', 'hearts', 'diams', 'quot', 'lt', 'gt', 'oelig', 'oelig', 'scaron', 
+            'scaron', 'Yuml', 'circ', 'tilde', 'ensp', 'emsp', 'thinsp', 'zwnj', 'zwj', 'lrm', 'rlm', 'ndash', 
+            'mdash', 'lsquo', 'rsquo', 'sbquo', 'ldquo', 'rdquo', 'bdquo', 'dagger', 'Dagger', 'permil', 'lsaquo', 'rsaquo', 'euro']
+
+for i in range(len(stopword_add)):
+    stopword_set.add(stopword_add[i])
+
+print(len(stopword_set))
+with open('stopword.pkl', 'wb') as f:
+    pkl.dump(set(stopword_set), f)
+    f.close()
