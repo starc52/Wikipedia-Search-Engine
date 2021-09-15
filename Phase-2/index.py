0c90f17a55baf75908df1e8e608d86ad3da05d66
starc52 <cstarpowful@gmail.com>
2021-09-15 06:36:56 +0530

    last straw

--- Phase-2/index.py
+++ Phase-2/index.py
@@ -45,15 +45,16 @@ with open('stopwords.pkl', 'rb') as f:
     f.close()
 stopword_set.add('category')
 
-string_garbage = re.compile(r"{{[^{}]*?}}")
-links_garbage = re.compile(r"(https?://[^| ]+)")
+string_garbage = re.compile(r"\[\[[^\[\]]*\]\]|{{[^{}]*?}}")
+links_garbage = re.compile(r'(https?://\S+)')#re.compile(r"(https?://[^| ]+)")
 token_reg = re.compile(r"[a-zA-Z0-9]+")
 number_garbage = re.compile(r"[0-9]+[a-z]+[0-9a-z]*")
 parse_references = re.compile(r"(\*?{{.*\|)(title=.*?)(\|.*?}})")
 parse_ext_links = re.compile(r"( *\* *\[[^ ]*)(.*?)(\])")
-parse_infobox=re.compile(r"{{infobox((.|\n)*)}}\n")
+css_reg = re.compile(r"{\|(.*?)\|}", re.DOTALL)
+# parse_infobox=re.compile(r"{{infobox *\n}}\n", re.DOTALL)
 # parse_infobox = re.compile(r"({{infobox.*)(\|.*)(\|.*?}}\n)")
-parse_categories =re.compile(r"\[\[category:[^\n ]*?\]\]")
+parse_categories =re.compile(r"\[\[category:.*?\]\]")
 
 def hms_string(sec_elapsed):
     h = int(sec_elapsed / (60 * 60))
@@ -69,6 +70,13 @@ def strip_tag_name(t):
         t = t[idx + 1:]
     return t
 
+def addTitle2Files(temp_title_dict):
+    global title_doc_count
+    file_path = os.path.join(PATH_INDEX, str(title_doc_count)+".json")
+    with open(file_path, "w") as f:
+        f.write(json.dumps(temp_title_dict, indent=0, separators=(",", ":")).replace("\n", ""))
+        f.close()
+    title_doc_count+=1
 
 def addTempIndex2Files(temp_inverted_index):
     for i in indexFilesList:
@@ -121,31 +129,25 @@ def addCount2Index(word, totalCount,field, freq):
         temp_inverted_index[key_word][word][field][totalCount]+=1
     
 
-def fineTuneRegex(listOfstrings):
-    returnListOfStrings=["" for i in range(len(listOfstrings))]
-    for i in range(len(listOfstrings)):
-        if listOfstrings[i][0]=="{":
-            bracket="{"
-            inBracket="}"
-        else:
-            bracket="["
-            inBracket="]"
-        stack=0
-        for j in range(len(listOfstrings[i])):
-            if listOfstrings[i][j]==bracket and listOfstrings[i][j+1]==bracket:
-                j+=1
-                stack+=1
-            if listOfstrings[i][j]==inBracket and listOfstrings[i][j+1]==inBracket:
-                j+=1
-                stack-=1
-                if stack ==0:
-                    returnListOfStrings[i]=listOfstrings[i][:j+1]
-                    break
-    while "" in returnListOfStrings:
-        returnListOfStrings.remove("")
-    return returnListOfStrings
+def bracketStack(strings):
+    returnString=""
+    bracket="{"
+    inBracket="}"
+    stack=1
+    for j in range(len(strings)):
+        if j+1<len(strings) and strings[j]==bracket and strings[j+1]==bracket:
+            j+=1
+            stack+=1
+        if j+1<len(strings) and strings[j]==inBracket and strings[j+1]==inBracket:
+            j+=1
+            stack-=1
+            if stack ==0:
+                returnString=strings[:j+1]
+                break
+    return returnString
 #-----------------------------------------------------FREQ variable
-freq = 500
+freqD = 500000
+freqT=100000
 for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
     tname = strip_tag_name(elem.tag)
 
@@ -157,16 +159,17 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
             text=''
             inrevision = False
             ns = 0
+            totalCount += 1
         elif tname == 'revision':
             # Do not pick up on revision id's
             inrevision = True
     else:
         if tname == 'title':
             title = elem.text
-            totalCount += 1
 
             if not title:
                 continue
+            temp_title_dict.update({totalCount:title})
             title_1 = title.lower()
             temp_title_1=re.findall(token_reg, title_1)
             temp_title=[i for i in temp_title_1 if i not in stopword_set]
@@ -175,7 +178,7 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
             for i in temp_title:
                 if len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                     stemmed_i = i
-                    addCount2Index(stemmed_i, totalCount, 't', freq)
+                    addCount2Index(stemmed_i, totalCount, 't', freqD)
 
         elif tname == 'id' and not inrevision:
             id = int(elem.text)
@@ -186,20 +189,31 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
             if not text:
                 continue
             text_1 = text.lower()
+            text_1 = re.sub(css_reg, '', text_1)
             complete_text = text_1
 
+            info_text=""
+            if len(re.findall(r"{{infobox", text_1))>0:
+                infoBoxContent=text_1.split("{{infobox")[1]
+                info_text=bracketStack(infoBoxContent)
+            reference_text=""
+            external_link_text=""
+            if len(re.findall(r"==references==", text_1))>0:
+                reference_text = text_1.split("==references==")[1]
+            if len(re.findall(r"==external links==", text_1))>0:
+                external_link_text = text_1.split("==external links==")[1]
+            
             text_split=text_1.split("\n")
             for i in range(len(text_split)):
                 if len(text_split[i])>2:
-                    if text_split[i][0:3]=="# 2": #or text_split[i][0]=='&':
+                    if text_split[i][0:3]=="# 2" or text_split[i][0:7]=='&lt;!--':
                         text_split[i]=""
             text="\n".join(text_split)
             text_1 = re.sub(string_garbage, '', text)
-            text_1 = re.sub(parse_infobox, '', text_1)
+            text_1 = text_1.replace(info_text, "")
+            text_1 = text_1.replace(reference_text, "")
+            text_1 = text_1.replace(external_link_text, "")
             text_1 = re.sub(parse_categories, '', text_1)
-            text_1 = re.sub(parse_references, '', text_1)
-            text_1 = re.sub(parse_ext_links, '', text_1)
-            text_1 = re.sub(links_garbage, '', text_1)
             text = re.sub(number_garbage, '', text_1)
             text_1 = re.findall(token_reg, text)
             text = [i for i in text_1 if i not in stopword_set]
@@ -210,7 +224,7 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
             for i in temp_text:
                 if len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                     stemmed_i = i
-                    addCount2Index(stemmed_i, totalCount, 'b', freq)
+                    addCount2Index(stemmed_i, totalCount, 'b', freqD)
             
             text_2 = complete_text
 
@@ -231,18 +245,13 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
                 for i in temp_cat_text:
                     if len(i)<20  and len(i)!=0:
                         stemmed_i = i
-                        addCount2Index(stemmed_i, totalCount, 'c', freq)
+                        addCount2Index(stemmed_i, totalCount, 'c', freqD)
 
             text_3 = complete_text
-            text_3_info = re.findall(parse_infobox, text_3)
-            if len(text_3_info)>0:
-                text_3 = [" ".join(i) for i in text_3_info]
-                text_3 = " ".join(fineTuneRegex(text_3))
-                text_3 = re.sub(links_garbage, '', text_3)
-
+            if len(info_text)>0:
+                text_3 = re.sub(links_garbage, '', info_text)
                 text_3 = re.sub(number_garbage, '', text_3)
                 text_3_info = re.findall(token_reg, text_3)
-
                 text_3 = [i for i in text_3_info if i not in stopword_set]
                 text_3 = removeNumbers(text_3)
                 
@@ -251,7 +260,7 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
                 for i in temp_info_text:
                     if len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                         stemmed_i = i
-                        addCount2Index(stemmed_i, totalCount, 'i', freq)
+                        addCount2Index(stemmed_i, totalCount, 'i', freqD)
                 
             text_4 = complete_text
             if len(re.findall(r"==references==", text_4))>0 and len(re.findall(r"==external links==", text_4))>0:
@@ -277,7 +286,7 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
                     for i in temp_ref_text:
                         if len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                             stemmed_i = i
-                            addCount2Index(stemmed_i, totalCount, 'r', freq)
+                            addCount2Index(stemmed_i, totalCount, 'r', freqD)
 
                 temp_ext_reg=re.findall(parse_ext_links, text_5_extlin)
                 if len(temp_ext_reg)>0:
@@ -294,7 +303,7 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
                     for i in temp_extlin_text:
                         if len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                             stemmed_i = i
-                            addCount2Index(stemmed_i, totalCount, 'l', freq)
+                            addCount2Index(stemmed_i, totalCount, 'l', freqD)
             
             elif len(re.findall(r"==references==", text_4))>0:
                 text_4_ref = text_4.split("==references==")[1]
@@ -317,7 +326,7 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
                     for i in temp_ref_text:
                         if len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                             stemmed_i = i
-                            addCount2Index(stemmed_i, totalCount, 'r', freq)
+                            addCount2Index(stemmed_i, totalCount, 'r', freqD)
             elif len(re.findall(r"==external links==", text_4))>0:
                 text_5_extlin = text_4.split("==external links==")[1]
                 temp_ext_reg=re.findall(parse_ext_links, text_5_extlin)
@@ -335,25 +344,29 @@ for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
                     for i in temp_extlin_text:
                         if len(i)<20 and i[-3:]!="jpg" and i[-4:]!="jpeg" and i[-3:]!="png" and len(i)!=0:
                             stemmed_i = i
-                            addCount2Index(stemmed_i, totalCount, 'l', freq)
+                            addCount2Index(stemmed_i, totalCount, 'l', freqD)
 
         elif tname == 'page':
             count_ns.append(ns)
             orig_text=text
-                        
-            if totalCount%freq==0:
+            if totalCount%freqT==0:
+                addTitle2Files(temp_title_dict)
+                temp_title_dict={}
+            if totalCount%freqD==0:
                 addTempIndex2Files(temp_inverted_index)
                 temp_inverted_index={}
                 for i in indexFilesList:
                     temp_inverted_index[i]={}
-                print(totalCount)               
-        
+            if totalCount%1000==0:
+                print(totalCount)        
         elem.clear()
 
 addTempIndex2Files(temp_inverted_index)
 temp_inverted_index={}
 for i in indexFilesList:
     temp_inverted_index[i]={}
+addTitle2Files(temp_title_dict)
+temp_title_dict={}
 
 total_keys = 0
 for i in indexFilesList:
